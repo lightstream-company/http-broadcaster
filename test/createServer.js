4d02427ac45305be1edb9ca3c272f8ba4a46ba14
Franck Ernewein <franck.ernewein@gmail.com>
2016-02-23 18:18:37 +0100

    chore: bootstrap

---
+++ test/createServer.js
@@ -0,0 +1,19 @@
+//const expect = require('chai').expect;
+
+describe('createServer', () => {
+
+  it('should raise a error because argument is missing');
+
+  it('should raise a error because no urls specified');
+
+  it('should forward over one service');
+
+  it('should forward over 2 services');
+
+  it('should get receive status 200');
+
+  it('should get receive status 500');
+
+  it('should get receive status NETWORK_ERROR');
+
+});
